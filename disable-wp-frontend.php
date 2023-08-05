d0895acc589386be41722ede8f3b4c6b48f1e92c
Fabian Reinders <fabian@fabiancdng.com>
2023-08-05 15:29:30 +0200

    🎉 Init

---
+++ disable-wp-frontend.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Plugin Name: Disable WP Frontend
+Plugin URI: https://github.com/fabiancdng/disable-wp-frontend
+Description: Disables the WordPress front end (public-facing part of the website). Leaves Dashboard, API, Media Uploads, and Cron untouched.
+Author: Fabian Reinders
+Author URI: https://fabiancdng.com
+Version: 1.0
+*/
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! function_exists( 'disable_wp_frontend' ) ) {
+	/**
+	 * Catch all front end requests and redirect them to wp-admin/wp-login.
+	 */
+	function disable_wp_frontend(): void {
+		// Check if request is not wp-admin, wp-login.
+		if ( ! is_admin() && ! is_login() ) {
+			// Make sure, the request is not a media file in wp-content (like an image).
+			// Allow all requests to wp-content/uploads/*.
+			if ( ! str_contains( $_SERVER['REQUEST_URI'], '/wp-content/uploads/' ) ) {
+				return;
+			}
+
+			// Make sure, the request is not a call to wp-cron.php.
+			if ( str_contains( $_SERVER['REQUEST_URI'], '/wp-cron.php' ) ) {
+				return;
+			}
+
+			// Redirect to wp-admin.
+			wp_redirect( site_url( 'wp-admin' ) );
+			exit;
+		}
+	}
+}
+
+// Hook up to the template_redirect action (so for instance REST API calls still work).
+add_action( 'template_redirect', 'disable_wp_frontend' );
