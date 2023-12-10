87c682b0a252f9ad04a980b7cc1ec48812c4d874
Fabian Reinders <fabian@fabiancdng.com>
2023-12-10 19:24:00 +0100

    ✨Add check for pathlist option

--- src/Plugin.php
+++ src/Plugin.php
@@ -28,21 +28,22 @@ class Plugin {
 		$plugin_settings = new SettingsPage();
 		$plugin_settings->register_settings_page();
 
-		// Instantiate the RedirectController handling the redirecting of front end requests.
-		$redirect_controller = new RedirectController();
+		// Only run the redirect logic if the 'disable_wp_frontend' option is set to true.
+		$disable_wp_frontend_settings = get_option( 'disable_wp_frontend_settings' );
+		if ( ! isset( $disable_wp_frontend_settings['general_settings']['disable_wp_frontend'] ) || true === $disable_wp_frontend_settings['general_settings']['disable_wp_frontend'] ) {
+			// Instantiate the RedirectController handling the redirecting of front end requests.
+			$redirect_controller = new RedirectController();
 
-		// If 'disable_wp_frontend_path_whitelist' option is set, use it to set the path whitelist.
-		$path_whitelist = get_option( 'disable_wp_frontend_settings' )['exception_settings']['path_whitelist'] ?? array(
-			'/wp-content/uploads/',
-			'/favicon.ico',
-			'/rss',
-			'/wp-cron.php',
-		);
-		if ( false !== $path_whitelist && is_array( $path_whitelist ) ) {
-			$redirect_controller->set_path_whitelist( $path_whitelist );
+			// If 'disable_wp_frontend_path_whitelist' option is set, use it to set the path whitelist.
+			$path_whitelist = get_option( 'disable_wp_frontend_settings' )['exception_settings']['path_whitelist'] ?? array(
+				'/favicon.ico',
+			);
+			if ( false !== $path_whitelist && is_array( $path_whitelist ) ) {
+				$redirect_controller->set_path_whitelist( $path_whitelist );
+			}
+
+			// Hook up to the template_redirect action (so for instance REST API calls still work).
+			add_action( 'template_redirect', array( $redirect_controller, 'redirect' ) );
 		}
-
-		// Hook up to the template_redirect action (so for instance REST API calls still work).
-		add_action( 'template_redirect', array( $redirect_controller, 'redirect' ) );
 	}
 }
--- src/SettingsPage.php
+++ src/SettingsPage.php
@@ -37,13 +37,13 @@ class SettingsPage {
 
 		// General settings section.
 		add_settings_section(
-			'disable_wp_frontend_general',
+			'disable_wp_frontend_general_settings',
 			'General settings',
 			array( $this, 'render_general_settings_section' ),
 			'disable-wp-frontend'
 		);
 
-		// Path Whitelist settings section.
+		// Exception settings section.
 		add_settings_section(
 			'disable_wp_frontend_exception_settings',
 			'Exception settings',
@@ -51,15 +51,41 @@ class SettingsPage {
 			'disable-wp-frontend'
 		);
 
+		// Settings section to restore default settings.
+		add_settings_section(
+			'disable_wp_frontend_restore_default_settings',
+			'Restore default settings',
+			array( $this, 'render_restore_default_settings_section' ),
+			'disable-wp-frontend'
+		);
+
 		// Register the settings fields.
 
-		// Path Whitelist settings field.
+		// 'Disable WP Frontend' settings field.
+		add_settings_field(
+			'disable_wp_frontend_general_settings_disable_wp_frontend',
+			'Disable WordPress Frontend',
+			array( $this, 'render_general_settings_disable_wp_frontend_field' ),
+			'disable-wp-frontend',
+			'disable_wp_frontend_general_settings'
+		);
+
+		// 'Path Whitelist' settings field.
 		add_settings_field(
 			'disable_wp_frontend_exception_settings_path_whitelist',
 			'Path Whitelist',
 			array( $this, 'render_exception_settings_path_whitelist_field' ),
 			'disable-wp-frontend',
 			'disable_wp_frontend_exception_settings'
+		);
+
+		// 'Restore default settings' settings field.
+		add_settings_field(
+			'disable_wp_frontend_restore_default_settings_restore_default_settings',
+			'Restore default settings',
+			array( $this, 'render_restore_default_settings_restore_default_settings_field' ),
+			'disable-wp-frontend',
+			'disable_wp_frontend_restore_default_settings'
 		);
 	}
 
@@ -96,8 +122,6 @@ class SettingsPage {
             <h1><?php esc_html_e( 'Disable WP Frontend Settings', 'disable-wp-frontend' ); ?></h1>
             <p><?php esc_html_e( 'Here, you can manage the settings for disabling the WordPress front end.', 'disable-wp-frontend' ); ?></p>
 
-			<?php settings_errors(); ?>
-
             <form action="options.php" method="post">
 				<?php
 				settings_fields( 'disable_wp_frontend_settings' );
@@ -113,6 +137,13 @@ class SettingsPage {
 	 * Renders the general settings section.
 	 */
 	public function render_general_settings_section(): void {
+		// If the 'disable_wp_frontend' option is set to true, display a warning and recommend to disable the plugin.
+		if ( isset( get_option( 'disable_wp_frontend_settings' )['general_settings']['disable_wp_frontend'] ) && false === get_option( 'disable_wp_frontend_settings' )['general_settings']['disable_wp_frontend'] ) {
+			?>
+            <p style="color: red;"><?php esc_html_e( 'Warning: The WordPress front end is currently enabled (see option below).', 'disable-wp-frontend' ); ?></p>
+            <p style="color: red; margin-bottom: 25px;"><?php esc_html_e( 'If you want to keep it enabled permanently, we recommend you to disable the plugin.', 'disable-wp-frontend' ); ?></p>
+			<?php
+		}
 		?>
         <p><?php esc_html_e( 'Here, you can manage the general settings for disabling the WP front end.', 'disable-wp-frontend' ); ?></p>
 		<?php
@@ -128,21 +159,41 @@ class SettingsPage {
 	}
 
 	/**
-	 * Renders the path whitelist field.
+	 * Renders the restore default settings section.
+	 */
+	public function render_restore_default_settings_section(): void {
+		?>
+        <p style="color: red; margin-bottom: 25px;"><?php esc_html_e( 'Warning: Checking the option below will delete all settings and restore default values!', 'disable-wp-frontend' ); ?></p>
+        <p><?php esc_html_e( 'Here, you can restore the default settings for disabling the WP front end.', 'disable-wp-frontend' ); ?></p>
+		<?php
+	}
+
+	/**
+	 * Renders the 'Disable WP Frontend' field.
+	 */
+	public function render_general_settings_disable_wp_frontend_field(): void {
+		$disable_wp_frontend = get_option( 'disable_wp_frontend_settings' )['general_settings']['disable_wp_frontend'] ?? true;
+		?>
+        <input type="checkbox" name="disable_wp_frontend_settings[general_settings][disable_wp_frontend]" id="disable_wp_frontend_general_settings_disable_wp_frontend"
+               value="1" <?php checked( $disable_wp_frontend, true ); ?> />
+        <label for="disable_wp_frontend_general_settings_disable_wp_frontend"><?php esc_html_e( 'Disable WordPress Frontend', 'disable-wp-frontend' ); ?></label>
+        <p class="description"><?php esc_html_e( 'Check this box to disable the WordPress frontend and redirect all pages to wp-admin or wp-login (except for the paths whitelisted below).', 'disable-wp-frontend' ); ?></p>
+		<?php
+	}
+
+	/**
+	 * Renders the 'Path Whitelist' field.
 	 */
 	public function render_exception_settings_path_whitelist_field(): void {
 		$path_whitelist = get_option( 'disable_wp_frontend_settings' )['exception_settings']['path_whitelist'] ?? array(
-			'/wp-content/uploads/',
 			'/favicon.ico',
-			'/rss',
-			'/wp-cron.php'
 		);
 		// Textarea field and paths separated by new line.
 		?>
         <textarea name="disable_wp_frontend_settings[exception_settings][path_whitelist]" id="disable_wp_frontend_exception_settings_path_whitelist" cols="65"
                   rows="15"><?php echo esc_textarea( implode( "\n", $path_whitelist ) ); ?></textarea>
 
-        <p class="description"><?php esc_html_e( 'Enter the paths that are allowed to be accessed besides wp-admin and wp-login.', 'disable-wp-frontend' ); ?></p>
+        <p class="description"><?php esc_html_e( 'Enter the paths that are allowed to be accessed besides some crucial internal WP pages.', 'disable-wp-frontend' ); ?></p>
         <p class="description"><?php esc_html_e( 'One path per line.', 'disable-wp-frontend' ); ?></p>
         <p class="description"><?php esc_html_e( 'Matching works by checking whether the path in the whitelist is contained in the request\'s URL.', 'disable-wp-frontend' ); ?></p>
         <p class="description"><?php esc_html_e( 'Hence, be careful with rules that are too general.', 'disable-wp-frontend' ); ?></p>
@@ -150,9 +201,31 @@ class SettingsPage {
 	}
 
 	/**
+	 * Renders the 'Restore default settings' field.
+	 */
+	public function render_restore_default_settings_restore_default_settings_field(): void {
+		// Red disclaimer that this will delete all settings.
+		?>
+        <input type="checkbox" name="disable_wp_frontend_settings[restore_default_settings][restore_default_settings]" id="disable_wp_frontend_restore_default_settings_restore_default_settings"
+               value="1"/>
+        <label for="disable_wp_frontend_restore_default_settings_restore_default_settings"><?php esc_html_e( 'Restore default settings', 'disable-wp-frontend' ); ?></label>
+        <p style="margin-bottom: 25px;"
+           class="description"><?php esc_html_e( 'Check this box and click save to restore the default settings for disabling the WP front end.', 'disable-wp-frontend' ); ?></p>
+		<?php
+	}
+
+	/**
 	 * Sanitizes the settings.
 	 */
 	public function sanitize_settings( array $settings ): array {
+		// If the restore default settings checkbox is checked, restore the default settings.
+		if ( isset( $settings['restore_default_settings']['restore_default_settings'] ) ) {
+			return array();
+		}
+
+		// Sanitize the general settings.
+		$settings['general_settings']['disable_wp_frontend'] = isset( $settings['general_settings']['disable_wp_frontend'] );
+
 		// Sanitize the path whitelist. Take in the string from the textarea and split it into an array by new line. Make sure the array items are sanitized.
 		$settings['exception_settings']['path_whitelist'] = array_map( 'sanitize_text_field', explode( "\n", $settings['exception_settings']['path_whitelist'] ) );
 
